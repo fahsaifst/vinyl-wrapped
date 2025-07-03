869494b60cdd77fcb2da239291131bf6e4cf4e6d
fahsaifst <98171729+fahsaifst@users.noreply.github.com>
2025-07-04 03:20:53 +0700

    Fix import contact

--- client/app/contact-top-tracks/page.tsx
+++ client/app/contact-top-tracks/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { ContactVinyl } from "../contact/page";
+import ContactVinyl from "../contact/page";
 
 export default function ContactPage() {
   const router = useRouter();

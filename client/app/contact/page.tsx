6390826b5141ce4054737703fc7902f037e8a588
fahsaifst <98171729+fahsaifst@users.noreply.github.com>
2025-07-04 03:20:53 +0700

    Fix import contact

--- client/app/contact/page.tsx
+++ client/app/contact/page.tsx
@@ -2,6 +2,7 @@
 
 import Navbar from "@/components/navbar";
 import {
+  ContactVinyl,
   LoginButtun,
   VinylCover,
 } from "@/components/VinylWrapped";
@@ -23,40 +24,8 @@ export default function ContactPage() {
   );
 }
 
-export const ContactVinyl = () => {
-  return (
-    <div className="min:w-screen flex items-center justify-center overflow-hidden">
-      <div className="w-[450px] h-[450px] sm:w-[330px] sm:h-[330px] rounded-full backdrop-blur-xs z-20 " />
 
-      <img
-        src="/Vinyl_BG.png"
-        alt="Vinyl Background"
-        className="absolute w-[550px] h-[550px] sm:w-[550px] sm:h-[550px] object-cover z-20"
-      />
 
-      <div className="absolute  h-70 z-20 flex flex-col items-center justify-between text-black text-center px-4 ">
-        <div>
-          <p className="text-[30px] sm:text-[28px] font-extrabold leading-none">
-            VINYL
-          </p>
-          <p className="text-[15px] sm:text-[16px] font-black leading-none mb-2">
-            WRAPPED
-          </p>
-        </div>
-        <div className="text-[10px] sm:text-[12px] font-medium flex gap-30">
-          <span className="font-bold">IG: Lilgupkao</span>
-          <span className="font-bold">IG: Fahsai.fst</span>
-        </div>
-        <div>
-          <img className="h-8 mb-2"
-          src="/barcode-tq.png" alt="thank you" />
-          <p className="text-[8px]">Made by</p>
-          <p className="text-[8px] font-semibold">Fahsai x Captain</p>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 // export const ContactVinyl = () => {
 //   return (
